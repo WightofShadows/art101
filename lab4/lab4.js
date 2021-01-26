ffbec6ec61bc98344b4c07f22709f47b0ba23ffc
WightofShadows <therewindcast@gmail.com>
2021-01-25 20:45:37 -0800

    lab 4 mostly

---
+++ lab4/lab4.js
@@ -0,0 +1,19 @@
+// "Draw" picture from chosen image:
+
+// Accept user image
+	// Identify image size/properties
+	// Download file
+// Scan chosen image
+	// Identify pixel count
+// Identify colors/pigments
+	// Coordinate unique color variations/hues
+		// Assign colors to corresponding pixels
+// User chooses “style” (filter)
+	// Present/load “style” options
+// Accept user input
+	// Load chosen “style” properties
+// Open blank file
+// Recreate/mimic colors/pigments according to “style”
+	// Load pixel/color configuration
+		// Apply pixel/color configuration according to “style” property
+// Load recreated image
